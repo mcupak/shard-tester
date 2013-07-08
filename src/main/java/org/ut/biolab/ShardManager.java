97b7e0b61dba1c34e1cd2c21969fe6cd1f6b0a9a
mcupak <mcupak@redhat.com>
2013-07-08 20:05:21 +0200

    added functionality for creating shards

---
+++ src/main/java/org/ut/biolab/ShardManager.java
@@ -0,0 +1,200 @@
+package org.ut.biolab;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Class for controlling shards.
+ * 
+ * @author <a href="mailto:mirocupak@gmail.com">Miroslav Cupak</a>
+ * 
+ */
+public class ShardManager {
+
+    /**
+     * Generate a table name for a given shard.
+     * 
+     * @param table
+     * @param index
+     * @return
+     */
+    private static String getShardName(String table, int index) {
+        return table + "_" + "s" + index;
+    }
+
+    /**
+     * Creates tables for shards based on the schema of the parent table.
+     * 
+     * @param c
+     * @param table
+     * @param shards
+     */
+    public static void createShards(Connection c, String table, int shards) {
+        PreparedStatement p;
+        for (int i = 0; i < shards; i++) {
+            // create table for a shard
+            try {
+                p = c.prepareStatement("CREATE TABLE IF NOT EXISTS " + getShardName(table, i) + " LIKE " + table);
+                System.out.println("Creating shard " + i);
+                p.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /**
+     * Determines the number of records in a table.
+     * 
+     * @param c
+     * @param table
+     * @return
+     */
+    public static int countRecords(Connection c, String table) {
+        int count = 1;
+        PreparedStatement s = null;
+        try {
+            s = c.prepareStatement("SELECT count(*) FROM " + table);
+            ResultSet co = s.executeQuery();
+            co.next();
+            count = co.getInt(1);
+        } catch (SQLException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } finally {
+            if (s != null) {
+                try {
+                    s.close();
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Fill shards with data from the original table.
+     * 
+     * @param c
+     * @param table
+     * @param shards
+     */
+    public static void fillShards(Connection c, String table, int shards) {
+        // do everything in a transaction to ensure consistency
+        PreparedStatement p = null;
+        try {
+            c.setAutoCommit(false);
+            int piece = countRecords(c, table);
+            c.commit();
+            for (int i = 0; i < shards; i++) {
+                // distribute the original table's data to shards round-robin
+                // (different strategy is also possible)
+                p = c.prepareStatement("INSERT INTO " + getShardName(table, i) + " SELECT * FROM " + table + " LIMIT " + piece + " OFFSET " + piece * i);
+                System.out.println("Creating shard " + i);
+                p.execute();
+            }
+            c.commit();
+            c.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (p != null) {
+                try {
+                    p.close();
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Fill shards with data from the original table. If your storage does not
+     * support direct insert (like ICE), use this instead of fillShards.
+     * 
+     * Be careful with this, ideally run with database running on localhost as
+     * it can transfer huge amounts of data.
+     * 
+     * Currently recycles a single file and performs operations in a serial
+     * manner. There is potential speedup if this part is parallellized,
+     * especially if you are testing on a machine with multiple hard disks.
+     * Howver, since sharding is usually only performed once and we are only
+     * interested in the time it takes to run the queries, we do not mind if
+     * this operation takes slightly more time than optimum.
+     * 
+     * @param c
+     * @param table
+     * @param shards
+     * @param buffer
+     */
+    public static void fillShardsViaFile(Connection c, String table, int shards, String file) {
+        PreparedStatement p = null;
+        File f = new File(file);
+        try {
+            int piece = (countRecords(c, table) + 1) / shards;
+            for (int i = 0; i < shards; i++) {
+                // distribute the original table's data to shards round-robin
+                // (different strategy is also possible)
+                System.out.println("Filling in shard " + i);
+                p = c.prepareStatement("SELECT * FROM " + table + " LIMIT " + piece + " OFFSET " + piece * i + " INTO OUTFILE '" + file + "' fields terminated by '\t'");
+                p.execute();
+                p = c.prepareStatement("LOAD DATA INFILE '" + file + "' INTO TABLE " + getShardName(table, i) + " fields terminated by '\t'");
+                p.execute();
+
+                // remove temp file
+                if (f.exists())
+                    f.delete();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (p != null) {
+                try {
+                    p.close();
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Gets rid of the tables.
+     * 
+     * @param c
+     * @param table
+     * @param shards
+     */
+    public static void cleanUp(Connection c, String table, int shards) {
+        if (shards > 1) {
+            PreparedStatement p = null;
+            for (int i = 0; i < shards; i++) {
+                // drop table
+                try {
+                    p = c.prepareStatement("DROP TABLE IF EXISTS " + getShardName(table, i));
+                    System.out.println("Deleting shard " + i);
+                    p.execute();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (p != null) {
+                try {
+                    p.close();
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/org/ut/biolab/Sharder.java
@@ -0,0 +1,134 @@
+package org.ut.biolab;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Main sharding class.
+ * 
+ * @author <a href="mailto:mirocupak@gmail.com">Miroslav Cupak</a>
+ */
+public class Sharder {
+
+    public static final String DEFAULT_CONFIG_FILE = "config.properties";
+    private static Properties config = new Properties();
+    private static Connection conn = null;
+    private static int shardCount = 0;
+
+    private static void loadProperties(Properties prop, String file) {
+        try {
+            prop.load(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            System.err.println("File " + file + " not found.");
+        } catch (IOException e) {
+            System.err.println("File " + file + " could not be read.");
+        }
+    }
+
+    /**
+     * Connects to the database.
+     * 
+     * @param host
+     * @param port
+     * @param database
+     * @param user
+     * @param password
+     * @return
+     */
+    public static Connection connect(String host, Integer port, String database, String user, String password) {
+        Connection c = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, user, password);
+        } catch (SQLException e) {
+            System.err.println("Could not connect to the database.");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.err.println("Could not connect to the database.");
+            e.printStackTrace();
+        }
+
+        return c;
+    }
+
+    /**
+     * Disconnects from the database.
+     * 
+     * @param c
+     */
+    public static void disconnect(Connection c) {
+        try {
+            c.close();
+        } catch (SQLException e) {
+            System.err.println("Could not close database connection.");
+        }
+    }
+
+    private static void testTable(Connection c, String table) {
+        // run simple select to test the connection and table
+        PreparedStatement testSelect = null;
+        try {
+            testSelect = c.prepareStatement("SELECT * FROM " + table + " LIMIT 2");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        ResultSet results;
+        try {
+            results = testSelect.executeQuery();
+            while (results.next()) {
+                System.out.println(results.getInt(3));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Controls the execution.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        // load properties from file
+        loadProperties(config, DEFAULT_CONFIG_FILE);
+
+        // parse command line args
+        // TODO: add the posibility to override properties from file via command
+        // line args (via gnu getopt)
+
+        // connect to DB
+        String host = config.getProperty("dbhost");
+        Integer port = Integer.valueOf(config.getProperty("dbport"));
+        String database = config.getProperty("dbname");
+        String user = config.getProperty("dbuser");
+        String password = config.getProperty("dbpassword");
+        conn = connect(host, port, database, user, password);
+
+        // create shards
+        String table = config.getProperty("dbtable");
+        shardCount = Integer.valueOf(config.getProperty("shardno"));
+        String file = "/tmp/buffer.tmp";
+        if (shardCount > 1) {
+            // create separate tables as shards
+            // ShardManager.cleanUp(conn, table, shardCount);
+            ShardManager.createShards(conn, table, shardCount);
+            ShardManager.fillShardsViaFile(conn, table, shardCount, file);
+        } else {
+            // use the original table
+        }
+        
+        
+
+        // disconnect
+        ShardManager.cleanUp(conn, table, shardCount);
+        disconnect(conn);
+    }
+}

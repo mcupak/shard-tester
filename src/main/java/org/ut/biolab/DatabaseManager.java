faa191a977315fff938af40e155c4b176cf35e4f
mcupak <mcupak@redhat.com>
2013-08-09 02:30:23 +0200

    added support for database export to files, refactoring

---
+++ src/main/java/org/ut/biolab/DatabaseManager.java
@@ -0,0 +1,130 @@
+/*
+ *    Copyright 2011-2012 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/*
+ *    Copyright 2011-2012 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.ut.biolab;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Manager of the database responsible for operations affecting database as a
+ * whole.
+ * 
+ * @author <a href="mailto:mirocupak@gmail.com">Miroslav Cupak</a>
+ * 
+ */
+public class DatabaseManager {
+    private static DatabaseManager instance = null;
+
+    protected DatabaseManager() {
+        // exists only to defeat instantiation.
+    }
+
+    public static DatabaseManager getInstance() {
+        if (instance == null) {
+            instance = new DatabaseManager();
+        }
+
+        return instance;
+    }
+
+    /**
+     * Constructs file name to store a table in.
+     * 
+     * @param table
+     *            table name
+     * @return file name
+     */
+    public static String getFileForTable(String table) {
+        return table + ".out";
+    }
+
+    /**
+     * Retrieves the list of tables in a database.
+     * 
+     * @param c
+     *            connection
+     * @return list of tables
+     */
+    public List<String> getTables(Connection c) {
+        List<String> tables = new ArrayList<String>();
+
+        try {
+            PreparedStatement p = c.prepareStatement("SHOW TABLES");
+            ResultSet rs = p.executeQuery();
+
+            while (rs.next()) {
+                tables.add(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            System.err.println("Could not retrieve tables.");
+        }
+
+        return tables;
+    }
+
+    /**
+     * Saves table to a file.
+     * 
+     * @param table
+     *            table to save
+     */
+    public void exportTable(Connection c, String table) {
+        System.out.println("Exporting table '" + table + "' into file '" + getFileForTable(table) + "'.");
+
+        PreparedStatement p;
+        try {
+            p = c.prepareStatement("SELECT * FROM " + table + " INTO OUTFILE '" + getFileForTable(table) + "' fields terminated by '\t'");
+            p.execute();
+        } catch (SQLException e) {
+            System.err.println("Failed to export table '" + table + "'.");
+        }
+
+        System.out.println("Export of table '" + table + "' finished.");
+    }
+
+    /**
+     * Exports all the tables in the database to files.
+     * 
+     * @param c
+     *            connection
+     */
+    public void exportDb(Connection c) {
+        List<String> tables = getTables(c);
+
+        for (String s : tables) {
+            exportTable(c, s);
+        }
+    }
+}
--- src/main/java/org/ut/biolab/Sharder.java
+++ src/main/java/org/ut/biolab/Sharder.java
@@ -29,6 +29,11 @@ public class Sharder {
     private static QueryExecutorManager qeManager = null;
     private static ConnectionManager cManager = null;
     private static Connection conn = null;
+    private static DatabaseManager dManager = null;
+
+    private enum Action {
+        EXPORTDB, SHARDTABLE, SHARDANDTESTTABLE;
+    }
 
     private static void loadProperties(Properties prop, String file) throws FileNotFoundException, IOException {
         prop.load(new FileInputStream(file));
@@ -47,6 +52,7 @@ public class Sharder {
     public static void connect(String host, Integer port, String database, String user, String password, int connNo) {
         cManager = ConnectionManager.getInstance();
         cManager.init(host, port, database, user, password, connNo);
+        dManager = DatabaseManager.getInstance();
         conn = cManager.getConnection();
     }
 
@@ -61,6 +67,78 @@ public class Sharder {
         } catch (SQLException e) {
             System.err.println("Could not close database connection.");
         }
+    }
+
+    /**
+     * Saves all the tables in a database into files.
+     */
+    public static void exportDb() {
+        System.out.println("Starting database export...");
+
+        dManager.exportDb(conn);
+
+        System.out.println("Database export  finished.");
+    }
+
+    /**
+     * Divides the given table into shards located in the same database.
+     * 
+     * @param table
+     *            table to shard
+     * @param file
+     *            file to use as a buffer
+     */
+    public static void shardTable(String table, String file) {
+        if (shardCount > 0) {
+            // create separate tables as shards
+            sManager.createShards(conn, table, shardCount);
+            sManager.fillShardsViaFile(conn, table, shardCount, file);
+        }
+    }
+
+    /**
+     * Shards a table and measures the performance of sample queries.
+     * 
+     * @param table
+     *            table to shard
+     * @param file
+     *            file to use as a buffer
+     */
+    public static void shardAndTestTable(String table, String file) {
+        // create shards
+        if (shardCount > 0) {
+            shardTable(table, file);
+
+            // schedule queries
+            qeManager = new QueryExecutorManager(shardCount);
+            List<String> queryBuffer = new ArrayList<String>();
+            queryBuffer.add(SELECT_STAR_TEMPLATE);
+            queryBuffer.add(COUNT_STAR_TEMPLATE);
+            queryBuffer.add(SINGLE_MATCH_WHERE_TEMPLATE);
+            queryBuffer.add(INTERVAL_TEMPLATE);
+            queryBuffer.add(PATTERN_TEMPLATE);
+
+            // run queries
+            for (String q : queryBuffer) {
+                System.out.println("Query: " + q);
+                List<Integer> results = qeManager.execute(q, table);
+
+                // aggregate results and measure the time it takes to merge
+                // trivial merging in this case for the purpose of
+                // comparison
+                QueryTimer qt = new QueryTimer();
+                qt.start();
+                int totalCount = 0;
+                for (Integer r : results) {
+                    totalCount += r;
+                }
+                qt.stop();
+                System.out.println("Result, merging time (ms): " + totalCount + ", " + qt.getDurationInMs());
+            }
+        }
+
+        // disconnect
+        sManager.cleanUp(conn, table, shardCount);
     }
 
     /**
@@ -87,6 +165,9 @@ public class Sharder {
             String user = config.getProperty("dbuser");
             String password = config.getProperty("dbpassword");
             shardCount = Integer.valueOf(config.getProperty("shardno"));
+            String table = config.getProperty("dbtable");
+            String file = config.getProperty("buffer");
+            Action action = Action.valueOf(config.getProperty("action"));
 
             // print config
             System.out.println("Host: " + host + ":" + port);
@@ -95,44 +176,22 @@ public class Sharder {
             // connect
             connect(host, port, database, user, password, shardCount + 1);
 
-            // create shards
-            String table = config.getProperty("dbtable");
-            String file = config.getProperty("buffer");
-            if (shardCount > 0) {
-                // create separate tables as shards
-                sManager.createShards(conn, table, shardCount);
-                sManager.fillShardsViaFile(conn, table, shardCount, file);
-
-                // schedule queries
-                qeManager = new QueryExecutorManager(shardCount);
-                List<String> queryBuffer = new ArrayList<String>();
-                queryBuffer.add(SELECT_STAR_TEMPLATE);
-                queryBuffer.add(COUNT_STAR_TEMPLATE);
-                queryBuffer.add(SINGLE_MATCH_WHERE_TEMPLATE);
-                queryBuffer.add(INTERVAL_TEMPLATE);
-                queryBuffer.add(PATTERN_TEMPLATE);
-
-                // run queries
-                for (String q : queryBuffer) {
-                    System.out.println("Query: " + q);
-                    List<Integer> results = qeManager.execute(q, table);
-
-                    // aggregate results and measure the time it takes to merge
-                    // trivial merging in this case for the purpose of
-                    // comparison
-                    QueryTimer qt = new QueryTimer();
-                    qt.start();
-                    int totalCount = 0;
-                    for (Integer r : results) {
-                        totalCount += r;
-                    }
-                    qt.stop();
-                    System.out.println("Result, merging time (ms): " + totalCount + ", " + qt.getDurationInMs());
-                }
+            // execute the goal
+            switch (action) {
+            case EXPORTDB:
+                exportDb();
+                break;
+            case SHARDTABLE:
+                shardTable(table, file);
+                break;
+            case SHARDANDTESTTABLE:
+                shardAndTestTable(table, file);
+                break;
+            default:
+                System.out.println("No valid action specified");
             }
 
             // disconnect
-            sManager.cleanUp(conn, table, shardCount);
             disconnect();
         } catch (FileNotFoundException e) {
             System.err.println("File " + DEFAULT_CONFIG_FILE + " not found.");
